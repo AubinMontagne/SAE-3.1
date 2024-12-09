5cc98056b16e92d9ef7fb510532ee83b2e9a0237
Jujedie <77742586+Jujedie@users.noreply.github.com>
2024-12-09 16:30:20 +0100

    Ajout des classes AssociationElement, QCM et Question pour améliorer la gestion des types de questions. Implémentation de méthodes pour gérer les associations et les réponses dans les classes correspondantes.

---
+++ src/Metier/QCM.java
@@ -0,0 +1,48 @@
+package Metier;
+
+import java.util.HashMap;
+
+
+public class QCM extends Question {
+    private HashMap<String, Boolean> reponses;
+
+    public QCM(String intitule, Difficulte difficulté,Notion notion,int temps,int points, Metier metier)
+    {
+        super(intitule, difficulté, notion, temps, points,metier);
+        this.reponses = new HashMap<String, Boolean>();
+    }
+    public QCM(String intitule, Difficulte difficulté,Notion notion,int temps,int points, Metier metier, String explication)
+    {
+        super(intitule, difficulté, notion, temps, points, metier, explication);
+        this.reponses = new HashMap<String, Boolean>();
+    }
+
+    // Get
+
+    public HashMap<String, Boolean> getReponses()
+    {
+        return this.reponses;
+    }
+
+    // Set
+
+    public void setReponses( HashMap<String, Boolean> reponses)
+    {
+        this.reponses = reponses;
+    }
+
+    // Méthode pour gérer les réponse ajouter/enlever
+    public void ajouterReponse(String reponse, Boolean correct)
+    {
+        this.reponses.put(reponse, correct);
+    }
+    public void enleverReponse(String reponse)
+    {
+        this.reponses.remove(reponse);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
