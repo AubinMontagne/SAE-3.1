b045c82b8ac6d467fc37ce679a063c7bd815a8ce
Julien Oyer <oyer.ludovic@gmail.com>
2024-12-10 12:45:04 +0100

    Ajout de la génération de fichiers RTF pour les questions, incluant l'intitulé, la difficulté, la notion, le temps, les points et l'explication. Refactorisation de la méthode getAsData pour écrire les données dans un fichier RTF et ajout de la gestion des exceptions.

--- src/Metier/Question.java
+++ src/Metier/Question.java
@@ -1,5 +1,14 @@
 package Metier;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.rtf.RTFEditorKit;
+
 public abstract class Question
 {
     private Metier     metier;
@@ -53,51 +62,78 @@ public abstract class Question
         this.point       = point;
     }
 
-    // Get
+    public String getAsData() {
+        String fileName = this.intitule.replaceAll("\\s+", "_") + ".rtf";
+        try {
+            DefaultStyledDocument doc = new DefaultStyledDocument();
+            StyleContext context = new StyleContext();
+            Style style = context.addStyle("Style", null);
+
+            // Ajouter l'intitulé de la question
+            style.addAttribute(StyleConstants.FontFamily, "Serif");
+            style.addAttribute(StyleConstants.Bold, true);
+            doc.insertString(doc.getLength(), "Intitulé: " + this.intitule + "\n", style);
+
+            // Ajouter la difficulté
+            style = context.addStyle("Style", null);
+            style.addAttribute(StyleConstants.Italic, true);
+            doc.insertString(doc.getLength(), "Difficulté: " + this.difficulte + "\n", style);
+
+            // Ajouter la notion
+            doc.insertString(doc.getLength(), "Notion: " + this.notion + "\n", style);
+
+            // Ajouter le temps
+            doc.insertString(doc.getLength(), "Temps: " + this.temps + " secondes\n", style);
+
+            // Ajouter les points
+            doc.insertString(doc.getLength(), "Points: " + this.point + "\n", style);
+
+            // Ajouter l'explication
+            doc.insertString(doc.getLength(), "Explication: " + this.explication + "\n", style);
+
+            // Écrire le document dans un fichier RTF
+            try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                RTFEditorKit rtfKit = new RTFEditorKit();
+                rtfKit.write(fos, doc, 0, doc.getLength());
+            }
+        } catch (IOException | BadLocationException e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }/*
+    public static Question getAsInstance(String fileName, Metier metier) {
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            DefaultStyledDocument doc = new DefaultStyledDocument();
+            RTFEditorKit rtfKit = new RTFEditorKit();
+            rtfKit.read(fis, doc, 0);
+
+            String content = doc.getText(0, doc.getLength());
+            String[] lines = content.split("\n");
+
+            String intitule = lines[0].split(": ")[1];
+            Difficulte difficulte = Difficulte.valueOf(lines[1].split(": ")[1]);
+            Notion notion = metier.getNotionById(Integer.parseInt(lines[2].split(": ")[1]));
+            int temps = Integer.parseInt(lines[3].split(": ")[1].split(" ")[0]);
+            int point = Integer.parseInt(lines[4].split(": ")[1]);
+            String explication = lines[5].split(": ")[1];
+
+            return new Question(intitule, difficulte, notion, temps, point, metier, explication);
+        } catch (IOException | BadLocationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }*/
+
+	// Get
     public String     getIntitule()    {return intitule;}
     public Difficulte getDifficulte()  {return this.difficulte;}
     public Notion     getNotion()      {return this.notion;}
     public int        getTemps()       {return this.temps;}
     public int        getPoint()       {return this.point;}
     public String     getExplication() {return this.explication;}
-
-    public String     getAsData(){
-        return (this.getClass().getName() + ";" + this.notion.getId() + ";" + this.intitule + ";" + this.difficulte.getIndice() + ";" + this.temps + ";" + this.point);
-    }
-    /*abstract Question getAsInstance(String ligne, Metier metier);
-    {
-        // Créer un document
-        DefaultStyledDocument doc = new DefaultStyledDocument();
-        StyleContext context = new StyleContext();
-        Style style = context.addStyle("Style", null);
-
-        // Ajouter du texte avec style
-        try {
-            style.addAttribute(StyleConstants.FontFamily, "Serif");
-            style.addAttribute(StyleConstants.Bold, true);
-            doc.insertString(doc.getLength(), "Texte en gras\n", style);
-
-            style = context.addStyle("Style", null);
-            style.addAttribute(StyleConstants.Italic, true);
-            doc.insertString(doc.getLength(), "Texte en italique\n", style);
-
-            style = context.addStyle("Style", null);
-            style.addAttribute(StyleConstants.Foreground, java.awt.Color.RED);
-            doc.insertString(doc.getLength(), "Texte en rouge\n", style);
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
-
-        // Écrire le document dans un fichier RTF
-        try (FileOutputStream fos = new FileOutputStream("output.rtf")) {
-            RTFEditorKit rtfKit = new RTFEditorKit();
-            rtfKit.write(fos, doc, 0, doc.getLength());
-        } catch (IOException | BadLocationException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-    // Set
+    
+	// Set
     public  void setIntitule(String intitule)          {this.intitule = intitule;}
     private void setDifficulte(Difficulte difficulte) {this.difficulte = difficulte;}
     private void setNotion(Notion notion)             {this.notion = notion;}
