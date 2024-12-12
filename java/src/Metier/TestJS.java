3f87fd1c203954e22894e4e024346a5cfa1a36c7
Julien Oyer <oyer.ludovic@gmail.com>
2024-12-12 16:30:24 +0100

    modifications sur TestJS

--- java/src/Metier/TestJS.java
+++ java/src/Metier/TestJS.java
@@ -7,10 +7,13 @@ public class TestJS
 
 		Ressource r1 = new Ressource("R3.01_Dev_WEB", "DevWEB");
 		Notion n1 = new Notion("Ecriture en PHP", r1);
-		Questionnaire questionnaire = new Questionnaire("Test1", r1 , false, new Metier());
+		Questionnaire questionnaire = new Questionnaire("Test1", r1 , false);
 		questionnaire.ajouterNotion(n1);
 		questionnaire.addQuestion(new QCM("Comment utiliser le PHP", Difficulte.MOYEN, n1, 20,2, "Pour aller coder en PHP vous devez d'abord lancer un serveur"));
-		questionnaire.getLstQuestion().get(0).
+		((QCM)questionnaire.getLstQuestion().get(0)).ajouterReponse("Docker", true);
+		((QCM)questionnaire.getLstQuestion().get(0)).ajouterReponse("XAMPP", true);
+		((QCM)questionnaire.getLstQuestion().get(0)).ajouterReponse("rien", false);
+		((QCM)questionnaire.getLstQuestion().get(0)).ajouterReponse("Test", false);
 
 		new QCMBuilder(questionnaire, "./");
 	}
