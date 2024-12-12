711ca5051832c50837dd5a0c8208499511096c69
PAUNETantoine <paunet.antoine@gmail.com>
2024-12-12 11:17:58 +0100

    Modif erreur

--- java/src/Metier/Questionnaire.java
+++ java/src/Metier/Questionnaire.java
@@ -156,6 +156,4 @@ public class Questionnaire {
       return var1;
    }
 }
- {
-	
-}
+
---
+++ java/src/Metier/TestJS.java
@@ -0,0 +1,10 @@
+package src.Metier;
+
+public class TestJS
+{
+	public static void main(String[] args)
+	{
+
+		Questionnaire questionnaire = new Questionnaire();
+	}
+}
